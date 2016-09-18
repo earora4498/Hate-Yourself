fb5c2b36524f55d24ac2f1dd084c125434703607
Jack Burgoyne <jburgoyne.98@gmail.com>
2016-09-18 10:07:59 -0400

    Now uses list of blocked websites

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,52 @@
+
+
+
 url = window.location.href;
-if (url.indexOf('facebook') !== -1) {
+
+chrome.storage.sync.get({
+	timerTime: 0,
+	site0: "",
+	site1: "",
+	site2: "",
+	site3: "",
+	site4: "",
+	site5: "",
+	site6: "",
+	site7: "",
+	site8: "",
+	site9: ""
+  }, function(items) {
+		if (url.indexOf(site0) !== -1) {
 	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
 }
+		if (url.indexOf(site1) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site2) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site3) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site4) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site5) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site6) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site7) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site8) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		if (url.indexOf(site9) !== -1) {
+	window.location = "chrome-extension://ggdcepfpnholabfkofncgdfghhggefdb/bad.html";
+}
+		
+		}
+  });
+  
